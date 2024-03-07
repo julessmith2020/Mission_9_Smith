a8d2e647779eee86f36371ca8076e4921cf32219
Jules Smith <jusmith@byu.edu>
2024-03-07 11:04:57 -0700

    mission 9 changes

--- blah/src/App.tsx
+++ blah/src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jsonData from "./CollegeBasketballTeams.json";
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+interface TeamData {
+  teams: TeamProps[];
+}
+
+const basketballTeams: TeamProps[] = (jsonData as TeamData).teams;
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div className="team-card">
+        <h2>{school.toUpperCase()}</h2>
+        <p>Mascot: {name}</p>
+        <p className="location">
+          Location: {city}, {state}
+        </p>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div className="team-list">
+      {basketballTeams.map((team, index) => (
+        <Team key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Jules was here
-        </a>
+      <header>
+        <h1 className="raleway-black">
+          COLLEGE
+          <br />
+          BASKETBALL
+          <br />
+          TEAMS
+        </h1>
       </header>
+      <TeamList />
     </div>
   );
 }
